35633f0042f16a6b9856760417b16704101fec70
David <davidk@mrcodeswildride.com>
2018-12-19 23:51:44 +0000

    removed comments from CSS and JS

--- script.js
+++
@@ -1,9 +0,0 @@
-// Section 1: Make separate variables for the text box, button, and paragraph.
-// You can name them anything you want, but for simplicity, name them the same as the IDs.
-
-
-// Section 2: Add an event listener to the button to show the story.
-// You can name the function anything you want, but pick something that indicates action, like showStory.
-
-
-// Section 3: Make a function to show the story.
